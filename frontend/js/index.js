8858a8dadbc7d1081a05b06a7cad32ad6bfde37f
Gosselke Loic <99717440+Gosselke-Loic@users.noreply.github.com>
2025-10-01 15:44:02 +0200

    Delete frontend directory

--- frontend/js/index.js
+++
@@ -1,6 +0,0 @@
-import stateManager from "./classes/StateManager.js"
-
-document.addEventListener("DOMContentLoaded", async () => {
-    //await stateManager.self();
-    stateManager.populate();
-})
